8c151fe451f1b31ebc6a2f76a7fbf8a119180be4
SpanishJoplin <jpreynoldsku@gmail.com>
2018-03-29 16:45:02 -0600

    final micro_blog

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-console.log("hello");
-
-
-
-
-
-
 const form = document.getElementById("blogForm");
 
 
 form.addEventListener("submit", function() {
 	event.preventDefault();
+
 	const inputValue = document.getElementById("blogInput").value;
 
-
-let newElement = document.createElement("p");
-	
-	newElement.innerHTML = inputValue;
-
-	document.body.appendChild(newElement);
+	//creating a new p tag
+	let newBlogPostElement = document.createElement("p");
+	//add input value to the <p> tag
+	newBlogPostElement.innerHTML = inputValue;
+	//appending to the web page
+	document.body.appendChild(newBlogPostElement);
+	//delete the value of the inputform
+	document.getElementById("blogInput").value='';
 
 
 });
@@ -27,3 +23,5 @@ let newElement = document.createElement("p");
 //1. removing the text in the entry form after the submit button is pressed
 //2. only show a couple past entries in the entry form
 //3. I also need to make sure the blog entries and stored in the localStorage so when you refresh the page it remembers what people said
+
+
